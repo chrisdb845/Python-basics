a15a24f8b8aaac0c2aa441dd4daa580d5c5df4f5
chrisdb845 <ckibet35@gmail.com>
2026-02-03 13:24:49 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,9 @@
+
+#Print is a method used to display an output
+
+
+print("Welcome to Python programming")
+print("Christian")
+print(200)
+print(40*3)
+
---
+++ variables.py
@@ -0,0 +1,16 @@
+#variable is a container used to store a value
+
+school = ("emobilis")
+firstname = "John"
+lastname = ("Smith")
+num1 = 45
+num2 = 100
+language = ("Python")
+
+
+print(school)
+print(firstname, lastname)
+print(lastname)
+print(school , " is a coding school")
+print(language , " is a web programming language")
+print("the sum of ", num1, "and" , num2 , "is" , num1 + num2)
